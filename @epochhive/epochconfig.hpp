9ac91a90b9e61a0b50c2e180c824c961abfe29c9
Denis Erygin <denisio.bro@gmail.com>
2015-03-26 14:33:15 +0300

    0.3.0.1

--- @epochhive/epochconfig.hpp
+++ @epochhive/epochconfig.hpp
@@ -5,19 +5,21 @@ BuildingSlotsLimit = 1500; // Max building slots allowed. Warning! Higher the nu
 StaticDateTime[] = {2035,6,10,7,0}; // {0,0,0,8,0} would forces the server to start at 8am each time it is started while allowing the year, month and day to stay real time. Any values left at 0 will result in no change.
 timeDifference = 0; // Server uses real time this will allow you to offset just the hour.
 timeMultiplier = 4; // Sets a time multiplier for in-game time. The command range is now capped at 0.1 - 120 to avoid performance problems.
-
+lootMultiplier = 0.5; // 1 = max loot bias 
 // Events
 WeatherChangeTime = 1200; // This controls how fast the weather changes as well as how fast shipping containers and earthquakes happen.
-SpecialEventTime = 2400;
-NormalEventTime = 1200;
-PlantSpawnTime = 2400;
-showEarthQuakes = true; // used to show locations of earthquakes
-showShippingContainers = true; // used to show locations of shipping containers
+WeatherStaticForecast[] = {}; // Default: {75.5,0,{0,0,0},0,{0,0}}; // Clear day; {19,1,{1,1,40},1,{5,5}}; // Cold Foggy Rainy Overcast Windy; Format: {temp <scalar>,rain <scalar>,fog <array>,overcast <scalar>,wind <array>} 
+events[] = {
+	{ 3600, "CarnivalSpawner" }, // SECOND <scalar>, EVENT <string>
+	{ 1200, "EarthQuake" },
+	{ 1200, "ContainerSpawner" },
+	{ 300, "PlantSpawner" } //No comma on last Entry
+};
 
 // Antagonists
-antagonistChanceTrash = 0.03; //3% chance when player loot a trash object
-antagonistChancePDeath = 0.25; //25% chance when player was killed from a other player (selfkill doesn't count)
-antagonistChanceLoot = 0.03; //3% chance when player click "SEARCH" on a loot object
+antagonistChanceTrash = 0.09; //9% chance when player loot a trash object
+antagonistChancePDeath = 0.33; //33% chance when player was killed from a other player (selfkill doesn't count)
+antagonistChanceLoot = 0.09; //9% chance when player click "SEARCH" on a loot object
 
 // Player Related
 cloneCost = 100; // debt incurred on player death
@@ -69,16 +71,20 @@ allowedVehiclesList[] = {
 
 // Traders
 taxRate = 0.1; // controls the price increase for purchases
-SHOW_TRADERS = true; // used to show locations of traders
+starterTraderItems[] = { { "ItemSodaBurst", "meatballs_epoch", "MortarBucket", "CinderBlocks", "VehicleRepair", "CircuitParts", "ItemCorrugated", "PartPlankPack", "ItemRock", "ItemRope", "ItemStick" }, { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 } }; // Starter Items for fresh spawned trader first array is classnames second is quantity.
 NPCSlotsLimit = 50; // Max number of traders static or dynamic. Warning! Higher the number lower performance.
 forceStaticTraders = true; // disables traders moving from work to home
+
+// Markers
+SHOW_TRADERS  = true; // Show locations of traders
+SHOW_JAMMERS  = true; // Shows location of base jammers 
+SHOW_BOATLOOT = true; // Shows the location of shipwreck loot 
+DEBUG_VEH     = false; // DEBUG ONLY used to debug spawing of vehicles
 
 // Hive Related
 expiresBuilding = "604800";  // expiration date in seconds for buildings
 expiresPlayer = "2592000";  // expiration date in seconds for players
 expiresBank = "7776000";  // expiration date in seconds for players bank
 expiresVehicle = "604800";  // expiration date in seconds for vehicles
+expiresAIdata = "604800";  // expiration date in seconds for NPC Trader inventory
 hiveAsync = false; // true = asynchronous hive calls (non blocking), false = synchronous hive calls (blocking)
-
-// Debug Related
-DEBUG_VEH = false; // DEBUG ONLY used to debug spawing of vehicles
